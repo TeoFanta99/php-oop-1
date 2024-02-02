a0da663df76ad7c17ae3f097229c5a9b9dd238a2
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-02-02 16:11:31 +0100

    tolti tutti gli echo e aggiunta una funzione manda a schermo il film

--- index.php
+++ index.php
@@ -12,40 +12,18 @@
             $this -> year = $year;
         }
 
-        public function getTitle() {
-            return $this -> title;
-        }
-
-        public function setTitle($title) {
-            $this -> title = $title; 
-        }
-
-        public function getGenres() {
-            return $this -> genres;
-        }
-
-        public function setGenres($genres) {
-            $this -> genres = $genres; 
+        public function showInfo() {
+            echo "Titolo: " . $this -> title . ", Genere: " . implode($this -> genres, ', ') . ", Anno: " . $this -> year;  
         }
     }
     
     $movie1 = new Movie('Titanic', array('Romantic', 'Dramatic'), '1997');
     $movie2 = new Movie('Truman Show', array('Comedy', 'Dramatic'), '1998');
 
-    echo '<h2>Film 1</h2>';
-    echo $movie1 -> title;
-    echo '<br>';
-    echo implode($movie1 -> genres, ', ');
-    echo '<br>';
-    echo $movie1 -> year;
+    $movie1 -> showInfo();
+    echo "<br>";
+    $movie2 -> showInfo();
 
-    echo '<br><br>';
 
-    echo '<h2>Film 2</h2>';
-    echo $movie2 -> title;
-    echo '<br>';
-    echo implode($movie2 -> genres, ', ' );
-    echo '<br>';
-    echo $movie2 -> year;
 
 ?>
